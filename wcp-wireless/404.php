a849d3410930ddacc3b18c20c41374be5ccb80d1
WCPMarketing <marketing@wcpwireless.com>
2026-09-15 12:20:47 -0400

    Simplify 404.php by removing redirect logic
    Removed legacy portal redirect logic and kept only the 404 page content.

--- wcp-wireless/404.php
+++ wcp-wireless/404.php
@@ -2,294 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
-| WCP LEGACY PORTAL REDIRECTS
+| WCP 404 PAGE
 |--------------------------------------------------------------------------
 |
-| Old WCP portal URLs are redirected to portal.wcpwireless.com.
-|
-| Incoming URLs are converted to lowercase for matching, so:
-|
-| /OREA
-| /orea
-| /Orea
-|
-| all match the same redirect.
-|
-| The destination capitalization is preserved exactly as entered below.
-|
-*/
-
-
-/*
-|--------------------------------------------------------------------------
-| GET REQUESTED PATH
-|--------------------------------------------------------------------------
-*/
-
-$request_uri = isset($_SERVER['REQUEST_URI'])
-    ? wp_unslash($_SERVER['REQUEST_URI'])
-    : '';
-
-
-$request_path = parse_url(
-    $request_uri,
-    PHP_URL_PATH
-);
-
-
-$request_path = rawurldecode(
-    $request_path
-);
-
-
-$request_path = trim(
-    $request_path,
-    '/'
-);
-
-
-$request_key = strtolower(
-    $request_path
-);
-
-
-/*
-|--------------------------------------------------------------------------
-| LEGACY PORTAL REDIRECT LIST
-|--------------------------------------------------------------------------
-*/
-
-$portal_redirects = array(
-
-    /*
-    |--------------------------------------------------------------------------
-    | NEW LOGIN REDIRECT
-    |--------------------------------------------------------------------------
-    */
-
-    'login' =>
-        'https://portal.wcpwireless.com/login',
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | EXISTING PORTAL REDIRECTS
-    |--------------------------------------------------------------------------
-    */
-
-    'vw' =>
-        'https://portal.wcpwireless.com/VW#Home',
-
-    'lakeridge' =>
-        'https://portal.wcpwireless.com/Lakeridge#Home',
-
-    'epp' =>
-        'https://portal.wcpwireless.com/EPP#Home',
-
-    'hospitalpromo1' =>
-        'https://portal.wcpwireless.com/hospitalpromo1#Home',
-
-    'uhn' =>
-        'https://portal.wcpwireless.com/UHN#Home',
-
-    'bph' =>
-        'https://portal.wcpwireless.com/BPH#Home',
-
-    'wohs' =>
-        'https://portal.wcpwireless.com/WOHS#Home',
-
-    'lhs' =>
-        'https://portal.wcpwireless.com/LHS#Home',
-
-    'sunnybrook' =>
-        'https://portal.wcpwireless.com/Sunnybrook#Home',
-
-    'hsc' =>
-        'https://portal.wcpwireless.com/HSC#Home',
-
-    'southlake' =>
-        'https://portal.wcpwireless.com/Southlake#Home',
-
-    'ocsb' =>
-        'https://portal.wcpwireless.com/ocsb#Home',
-
-    'promo1' =>
-        'https://portal.wcpwireless.com/promo1#Home',
-
-    'bee-clean' =>
-        'https://portal.wcpwireless.com/bee-clean#Home',
-
-    'peel' =>
-        'https://portal.wcpwireless.com/Peel#Home',
-
-    'promo3' =>
-        'https://portal.wcpwireless.com/promo3#Home',
-
-    'peelsb' =>
-        'https://portal.wcpwireless.com/Peelsb#Home',
-
-    'hrh' =>
-        'https://portal.wcpwireless.com/HRH#Home',
-
-    'msh' =>
-        'https://portal.wcpwireless.com/MSH#Home',
-
-    'publicsector1' =>
-        'https://portal.wcpwireless.com/publicsector1#Home',
-
-    'yrp' =>
-        'https://portal.wcpwireless.com/YRP#Home',
-
-    'rppoffer' =>
-        'https://portal.wcpwireless.com/rppoffer#Home',
-
-    'healthpromo' =>
-        'https://portal.wcpwireless.com/healthpromo#Home',
-
-    'magna' =>
-        'https://portal.wcpwireless.com/Magna#Home',
-
-    'hanon' =>
-        'https://portal.wcpwireless.com/Hanon#Home',
-
-    'litens' =>
-        'https://portal.wcpwireless.com/litens#Home',
-
-    'magnarpp' =>
-        'https://portal.wcpwireless.com/Magnarpp#Home',
-
-    'multimatic' =>
-        'https://portal.wcpwireless.com/Multimatic#Home',
-
-    'multimaticrpp' =>
-        'https://portal.wcpwireless.com/Multimaticrpp#Home',
-
-    'hospitalpromo3' =>
-        'https://portal.wcpwireless.com/HOSPITALPROMO3#Home',
-
-    'nygh' =>
-        'https://portal.wcpwireless.com/NYGH#Home',
-
-    'promo2' =>
-        'https://portal.wcpwireless.com/promo2#Home',
-
-    'selectrpp' =>
-        'https://portal.wcpwireless.com/SelectRPP#Home',
-
-    'lookup' =>
-        'https://portal.wcpwireless.com/lookup#Home',
-
-    'fblookup' =>
-        'https://portal.wcpwireless.com/fblookup#Home',
-
-    'eligibility' =>
-        'https://portal.wcpwireless.com/Eligibility#Home',
-
-    'lflgroup' =>
-        'https://portal.wcpwireless.com/LFLgroup#Home',
-
-    'facebook' =>
-        'https://portal.wcpwireless.com/Facebook#Home',
-
-    'wcprpp' =>
-        'https://portal.wcpwireless.com/WCPRPP#Home',
-
-    'meta' =>
-        'https://portal.wcpwireless.com/Meta#Home',
-
-    'fbrpp' =>
-        'https://portal.wcpwireless.com/FBRPP#Home',
-
-    'hsbc' =>
-        'https://portal.wcpwireless.com/HSBC#Home',
-
-    'crea' =>
-        'https://portal.wcpwireless.com/CREA#Home',
-
-    'kinark' =>
-        'https://portal.wcpwireless.com/kinark#Home',
-
-    'onhospital' =>
-        'https://portal.wcpwireless.com/ONhospital#Home',
-
-    'fbhospital' =>
-        'https://portal.wcpwireless.com/FBhospital#Home',
-
-    'orea' =>
-        'https://portal.wcpwireless.com/OREA#Home',
-
-    'fborea' =>
-        'https://portal.wcpwireless.com/FBOREA#Home',
-
-    'clar' =>
-        'https://portal.wcpwireless.com/CLAR#Home',
-
-    'rpp' =>
-        'https://portal.wcpwireless.com/RPP#Home',
-
-    'fedex' =>
-        'https://portal.wcpwireless.com/FEDEX#Home',
-
-    'fbpublicsector' =>
-        'https://portal.wcpwireless.com/FBPUBLICSECTOR#Home',
-
-    'rogersselect' =>
-        'https://portal.wcpwireless.com/RogersSelect#Home',
-
-    'td' =>
-        'https://portal.wcpwireless.com/TD#Home',
-
-    'bankrpp' =>
-        'https://portal.wcpwireless.com/BankRPP#Home',
-
-    'rbcepp' =>
-        'https://portal.wcpwireless.com/RBCEPP#Home',
-
-    'empoffers' =>
-        'https://portal.wcpwireless.com/EMPOFFERS#Home',
-
-);
-
-
-/*
-|--------------------------------------------------------------------------
-| RUN LEGACY REDIRECT
-|--------------------------------------------------------------------------
-*/
-
-if (
-    $request_key !== '' &&
-    isset($portal_redirects[$request_key])
-) {
-
-    $destination =
-        $portal_redirects[$request_key];
-
-
-    wp_redirect(
-        $destination,
-        301,
-        'WCP Legacy Portal Redirect'
-    );
-
-
-    exit;
-
-}
-
-
-/*
-|--------------------------------------------------------------------------
-| NORMAL WORDPRESS 404
-|--------------------------------------------------------------------------
-|
-| If the URL isn't one of the legacy portal URLs above,
-| show the normal WCP branded 404 page.
+| Portal shortcut redirects are handled centrally in functions.php through
+| template_redirect. This file now handles only genuine 404 responses.
 |
 */
 
 status_header(404);
-
 nocache_headers();
 
 get_header();
@@ -318,7 +39,6 @@ get_header();
         "
     >
 
-
         <div
             style="
                 font-size:15px;
@@ -332,11 +52,9 @@ get_header();
             404
         </div>
 
-
         <h1>
             Page Not Found
         </h1>
-
 
         <p
             class="lede"
@@ -349,7 +67,6 @@ get_header();
             The page you're looking for may have moved or no longer exists.
         </p>
 
-
         <div
             class="actions"
             style="
@@ -358,14 +75,12 @@ get_header();
             "
         >
 
-
             <a
                 href="<?php echo esc_url(home_url('/')); ?>"
                 class="btn btn-primary"
             >
                 Back to Home
             </a>
-
 
             <a
                 href="<?php echo esc_url(home_url('/contact/')); ?>"
@@ -374,9 +89,7 @@ get_header();
                 Contact Us
             </a>
 
-
         </div>
-
 
     </div>
 
