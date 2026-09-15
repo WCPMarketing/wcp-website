8341f78713b6adf0151f073884e7f80c8210d6f0
WCPMarketing <marketing@wcpwireless.com>
2026-09-15 12:19:58 -0400

    Add WCP portal shortcut redirects function

--- wcp-wireless/functions.php
+++ wcp-wireless/functions.php
@@ -6610,6 +6610,242 @@ function wcp_global_phone_href() {
 }
 /*
 |--------------------------------------------------------------------------
+| WCP PORTAL SHORTCUT REDIRECTS
+|--------------------------------------------------------------------------
+|
+| Public shortcut URLs on wcpwireless.com redirect directly to the WCP
+| portal before WordPress loads a page template or the 404 template.
+|
+| Matching is case-insensitive and ignores leading/trailing slashes.
+| Destination capitalization and URL fragments are preserved exactly below.
+|
+*/
+
+function wcp_portal_shortcut_redirects() {
+
+    $request_uri = isset($_SERVER['REQUEST_URI'])
+        ? wp_unslash($_SERVER['REQUEST_URI'])
+        : '';
+
+    $request_path = wp_parse_url(
+        $request_uri,
+        PHP_URL_PATH
+    );
+
+    $request_path = rawurldecode(
+        (string) $request_path
+    );
+
+    $request_key = strtolower(
+        trim($request_path, '/')
+    );
+
+    if ($request_key === '') {
+        return;
+    }
+
+    $portal_redirects = array(
+
+        'login' =>
+            'https://portal.wcpwireless.com/login',
+
+        'vw' =>
+            'https://portal.wcpwireless.com/VW#Home',
+
+        'lakeridge' =>
+            'https://portal.wcpwireless.com/Lakeridge#Home',
+
+        'epp' =>
+            'https://portal.wcpwireless.com/EPP#Home',
+
+        'hospitalpromo1' =>
+            'https://portal.wcpwireless.com/hospitalpromo1#Home',
+
+        'uhn' =>
+            'https://portal.wcpwireless.com/UHN#Home',
+
+        'bph' =>
+            'https://portal.wcpwireless.com/BPH#Home',
+
+        'wohs' =>
+            'https://portal.wcpwireless.com/WOHS#Home',
+
+        'lhs' =>
+            'https://portal.wcpwireless.com/LHS#Home',
+
+        'sunnybrook' =>
+            'https://portal.wcpwireless.com/Sunnybrook#Home',
+
+        'hsc' =>
+            'https://portal.wcpwireless.com/HSC#Home',
+
+        'southlake' =>
+            'https://portal.wcpwireless.com/Southlake#Home',
+
+        'ocsb' =>
+            'https://portal.wcpwireless.com/ocsb#Home',
+
+        'promo1' =>
+            'https://portal.wcpwireless.com/promo1#Home',
+
+        'bee-clean' =>
+            'https://portal.wcpwireless.com/bee-clean#Home',
+
+        'peel' =>
+            'https://portal.wcpwireless.com/Peel#Home',
+
+        'promo3' =>
+            'https://portal.wcpwireless.com/promo3#Home',
+
+        'peelsb' =>
+            'https://portal.wcpwireless.com/Peelsb#Home',
+
+        'hrh' =>
+            'https://portal.wcpwireless.com/HRH#Home',
+
+        'msh' =>
+            'https://portal.wcpwireless.com/MSH#Home',
+
+        'publicsector1' =>
+            'https://portal.wcpwireless.com/publicsector1#Home',
+
+        'yrp' =>
+            'https://portal.wcpwireless.com/YRP#Home',
+
+        'rppoffer' =>
+            'https://portal.wcpwireless.com/rppoffer#Home',
+
+        'healthpromo' =>
+            'https://portal.wcpwireless.com/healthpromo#Home',
+
+        'magna' =>
+            'https://portal.wcpwireless.com/Magna#Home',
+
+        'hanon' =>
+            'https://portal.wcpwireless.com/Hanon#Home',
+
+        'litens' =>
+            'https://portal.wcpwireless.com/litens#Home',
+
+        'magnarpp' =>
+            'https://portal.wcpwireless.com/Magnarpp#Home',
+
+        'multimatic' =>
+            'https://portal.wcpwireless.com/Multimatic#Home',
+
+        'multimaticrpp' =>
+            'https://portal.wcpwireless.com/Multimaticrpp#Home',
+
+        'hospitalpromo3' =>
+            'https://portal.wcpwireless.com/HOSPITALPROMO3#Home',
+
+        'nygh' =>
+            'https://portal.wcpwireless.com/NYGH#Home',
+
+        'promo2' =>
+            'https://portal.wcpwireless.com/promo2#Home',
+
+        'selectrpp' =>
+            'https://portal.wcpwireless.com/SelectRPP#Home',
+
+        'lookup' =>
+            'https://portal.wcpwireless.com/lookup#Home',
+
+        'fblookup' =>
+            'https://portal.wcpwireless.com/fblookup#Home',
+
+        'eligibility' =>
+            'https://portal.wcpwireless.com/Eligibility#Home',
+
+        'lflgroup' =>
+            'https://portal.wcpwireless.com/LFLgroup#Home',
+
+        'facebook' =>
+            'https://portal.wcpwireless.com/Facebook#Home',
+
+        'wcprpp' =>
+            'https://portal.wcpwireless.com/WCPRPP#Home',
+
+        'meta' =>
+            'https://portal.wcpwireless.com/Meta#Home',
+
+        'fbrpp' =>
+            'https://portal.wcpwireless.com/FBRPP#Home',
+
+        'hsbc' =>
+            'https://portal.wcpwireless.com/HSBC#Home',
+
+        'crea' =>
+            'https://portal.wcpwireless.com/CREA#Home',
+
+        'kinark' =>
+            'https://portal.wcpwireless.com/kinark#Home',
+
+        'onhospital' =>
+            'https://portal.wcpwireless.com/ONhospital#Home',
+
+        'fbhospital' =>
+            'https://portal.wcpwireless.com/FBhospital#Home',
+
+        'orea' =>
+            'https://portal.wcpwireless.com/OREA#Home',
+
+        'fborea' =>
+            'https://portal.wcpwireless.com/FBOREA#Home',
+
+        'clar' =>
+            'https://portal.wcpwireless.com/CLAR#Home',
+
+        'rpp' =>
+            'https://portal.wcpwireless.com/RPP#Home',
+
+        'fedex' =>
+            'https://portal.wcpwireless.com/FEDEX#Home',
+
+        'fbpublicsector' =>
+            'https://portal.wcpwireless.com/FBPUBLICSECTOR#Home',
+
+        'rogersselect' =>
+            'https://portal.wcpwireless.com/RogersSelect#Home',
+
+        'td' =>
+            'https://portal.wcpwireless.com/TD#Home',
+
+        'bankrpp' =>
+            'https://portal.wcpwireless.com/BankRPP#Home',
+
+        'rbcepp' =>
+            'https://portal.wcpwireless.com/RBCEPP#Home',
+
+        'empoffers' =>
+            'https://portal.wcpwireless.com/EMPOFFERS#Home',
+
+        'magnacorp' =>
+            'https://portal.wcpwireless.com/magnacorp',
+    );
+
+    if (!isset($portal_redirects[$request_key])) {
+        return;
+    }
+
+    wp_redirect(
+        $portal_redirects[$request_key],
+        301,
+        'WCP Portal Shortcut Redirect'
+    );
+
+    exit;
+}
+
+add_action(
+    'template_redirect',
+    'wcp_portal_shortcut_redirects',
+    1
+);
+
+
+/*
+|--------------------------------------------------------------------------
 | FORM HANDLERS & WCP SUBMISSIONS
 |--------------------------------------------------------------------------
 */
